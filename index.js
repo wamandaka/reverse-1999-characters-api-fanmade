a4f7f310cd8581d392e60e240d6e74d140e5fe96
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-12-18 01:01:42 +0700

    update cors

--- index.js
+++ index.js
@@ -5,26 +5,38 @@ const morgan = require("morgan");
 const session = require("express-session");
 const flash = require("express-flash");
 const router = require("./routes/route");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(session({
-  secret: "secret",
-  resave: false,
-  saveUninitialized: true
-}))
+// Setel opsi CORS sesuai kebutuhan Anda
+const corsOptions = {
+  origin: "http://localhost:3000", // Gantilah dengan origin aplikasi frontend Anda
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-const passport = require('./lib/passport');
+app.use(cors(corsOptions));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+const passport = require("./lib/passport");
 app.use(passport.initialize());
 app.use(passport.session());
 // app.use(flash());
 
-const authUser = require('./routes/auth_route')
-app.use(authUser)
+const authUser = require("./routes/auth_route");
+app.use(authUser);
 
-app.use("/", router)
+app.use("/", router);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
