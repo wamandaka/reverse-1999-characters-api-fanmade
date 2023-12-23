122dde3ac48c3dbb798d332392e82d75410f772c
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-12-23 22:45:21 +0700

    23/12/2023

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 // Misalnya, dalam Express.js
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Gantilah dengan origin aplikasi frontend Anda
+  res.header('Access-Control-Allow-Origin', true); // Gantilah dengan origin aplikasi frontend Anda
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Credentials', 'true');
